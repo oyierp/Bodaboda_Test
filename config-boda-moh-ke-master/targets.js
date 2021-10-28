cfec7489f18f433097db3fcda2f95984106fa902
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2021-10-28 11:26:50 +0300

    Add files via upload

--- config-boda-moh-ke-master/targets.js
+++ config-boda-moh-ke-master/targets.js
@@ -1,4 +1,45 @@
 module.exports = [
+{
+    id: 'assessments-all-time',
+    type: 'count',
+    icon: 'icon-healthcare-assessment',
+    goal: -1,
+    translation_key: 'targets.assessments.title',
+    subtitle_translation_key: 'targets.all_time.subtitle',
+
+    appliesTo: 'reports',
+    appliesToType: ['assessment'],
+    date: 'now'
+  }
 ];
+
+module.exports = [
+  {
+    id: 'assessments-all-time',
+    type: 'count',
+    icon: 'icon-healthcare-assessment',
+    goal: -1,
+    translation_key: 'targets.assessments.title',
+    subtitle_translation_key: 'targets.all_time.subtitle',
+
+    appliesTo: 'reports',
+    appliesToType: ['assessment'],
+    date: 'now'
+  },
+  {
+    id: 'assessments-this-month',
+    type: 'count',
+    icon: 'icon-healthcare-assessment',
+    goal: -1,
+    translation_key: 'targets.assessments.title',
+    subtitle_translation_key: 'targets.this_month.subtitle',
+
+    appliesTo: 'reports',
+    appliesToType: ['assessment'],
+    date: 'reported'
+  }
+];
+
+
 
 
