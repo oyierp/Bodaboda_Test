876fc8ee2a0a0852883c774b781ccfae3ddc5c1e
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2021-10-28 12:24:36 +0300

    Add files via upload

--- config-boda-moh-ke-master/targets.js
+++ config-boda-moh-ke-master/targets.js
@@ -1,19 +1,4 @@
 module.exports = [
-{
-    id: 'assessments-all-time',
-    type: 'count',
-    icon: 'icon-healthcare-assessment',
-    goal: -1,
-    translation_key: 'targets.assessments.title',
-    subtitle_translation_key: 'targets.all_time.subtitle',
-
-    appliesTo: 'reports',
-    appliesToType: ['assessment'],
-    date: 'now'
-  }
-];
-
-module.exports = [
   {
     id: 'assessments-all-time',
     type: 'count',
