a83070421b599721c74aee4beac451ac3205f235
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2021-11-18 17:18:11 +0300

    Add files via upload

--- config-boda-moh-ke-master/targets.js
+++ config-boda-moh-ke-master/targets.js
@@ -1,48 +1,48 @@
 module.exports = [
-  {
-    id: 'consent2',
-    type: 'count',
-    icon: 'icon-healthcare-assessment',
-    goal: 5000,
-    translation_key: 'targets.consent.title',
-    subtitle_translation_key: 'targets.all_time.subtitle',
+{
+  id: 'consent2',
+  type: 'count',
+  icon: 'icon-healthcare-assessment',
+  goal: 5000,
+  translation_key: 'targets.consent.title',
+  subtitle_translation_key: 'targets.all_time.subtitle',
 
-    appliesTo: 'contacts',
-    date: 'now'
+  appliesTo: 'contacts',
+  date: 'now'
   },
   {
-    id: 'cov_test',
-    type: 'count',
-    icon: 'icon-healthcare-assessment',
-    goal: -1,
-    translation_key: 'targets.covid_test.title',
-    subtitle_translation_key: 'targets.this_month.subtitle',
+  id: 'cov_test',
+  type: 'count',
+  icon: 'icon-healthcare-assessment',
+  goal: -1,
+  translation_key: 'targets.covid_test.title',
+  subtitle_translation_key: 'targets.this_month.subtitle',
 
-    appliesTo: 'reports',
-    date: 'reported'
+  appliesTo: 'reports',
+  date: 'reported'
   },
 
 {
-    id: 'tb_test',
-    type: 'count',
-    icon: 'icon-healthcare-assessment',
-    goal: -1,
-    translation_key: 'targets.tb_test.title',
-    subtitle_translation_key: 'targets.this_month.subtitle',
+  id: 'tb_test',
+  type: 'count',
+  icon: 'icon-healthcare-assessment',
+  goal: -1,
+  translation_key: 'targets.tb_test.title',
+  subtitle_translation_key: 'targets.this_month.subtitle',
 
-    appliesTo: 'reports',
-    date: 'reported'
+  appliesTo: 'reports',
+  date: 'reported'
   },
 {
-    id: 'ag_test',
-    type: 'count',
-    icon: 'icon-healthcare-assessment',
-    goal: -1,
-    translation_key: 'targets.ag_test.title',
-    subtitle_translation_key: 'targets.this_month.subtitle',
-
-    appliesTo: 'reports',
-    date: 'reported'
+  id: 'ag_test',
+  type: 'count',
+  icon: 'icon-healthcare-assessment',
+  goal: -1,
+  translation_key: 'targets.ag_test.title',
+  subtitle_translation_key: 'targets.this_month.subtitle',
+  
+  appliesTo: 'reports',
+  date: 'reported'
   },
 
 ];
