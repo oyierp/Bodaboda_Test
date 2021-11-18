a30ae122c3f9769b7955335bd7893d2a41c752a0
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2021-11-18 17:07:12 +0300

    Add files via upload

--- config-boda-moh-ke-master/targets.js
+++ config-boda-moh-ke-master/targets.js
@@ -1,18 +1,18 @@
 module.exports = [
   {
-    id: 'assessments-all-time',
+    id: 'consent2',
     type: 'count',
     icon: 'icon-healthcare-assessment',
-    goal: -1,
+    goal: 5000,
     translation_key: 'targets.assessments.title',
     subtitle_translation_key: 'targets.all_time.subtitle',
 
-    appliesTo: 'reports',
-    appliesToType: ['assessment'],
+    appliesTo: 'consenting',
+    appliesToType: ['contacts'],
     date: 'now'
   },
   {
-    id: 'assessments-this-month',
+    id: 'cov_test',
     type: 'count',
     icon: 'icon-healthcare-assessment',
     goal: -1,
@@ -22,7 +22,33 @@ module.exports = [
     appliesTo: 'reports',
     appliesToType: ['assessment'],
     date: 'reported'
-  }
+  },
+
+{
+    id: 'tb_test',
+    type: 'count',
+    icon: 'icon-healthcare-assessment',
+    goal: -1,
+    translation_key: 'targets.assessments.title',
+    subtitle_translation_key: 'targets.this_month.subtitle',
+
+    appliesTo: 'reports',
+    appliesToType: ['assessment'],
+    date: 'reported'
+  },
+{
+    id: 'ag_test',
+    type: 'count',
+    icon: 'icon-healthcare-assessment',
+    goal: -1,
+    translation_key: 'targets.assessments.title',
+    subtitle_translation_key: 'targets.this_month.subtitle',
+
+    appliesTo: 'reports',
+    appliesToType: ['assessment'],
+    date: 'reported'
+  },
+
 ];
 
 
