f8b5d4913c2b0dbd5b575e308e7bd51520609cbc
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2021-11-19 10:11:27 +0300

    Add files via upload

--- config-boda-moh-ke-master/targets.js
+++ config-boda-moh-ke-master/targets.js
@@ -1,47 +1,15 @@
 module.exports = [
-{
-    id: 'consent2',
-    type: 'count',
-    icon: 'icon-healthcare-assessment',
-    goal: 5000,
-    translation_key: 'targets.consent.title',
-    subtitle_translation_key: 'targets.all_time.subtitle',
-
-    appliesTo: 'contacts',
-    date: 'now'
-  },
   {
-    id: 'cov_test',
-    type: 'count',
+    id: 'percentage-covid-test',
+    type: 'percent',
     icon: 'icon-healthcare-assessment',
     goal: -1,
     translation_key: 'targets.covid_test.title',
     subtitle_translation_key: 'targets.this_month.subtitle',
 
-    appliesTo: 'reports',
-    date: 'reported'
-  },
-
-{
-    id: 'tb_test',
-    type: 'count',
-    icon: 'icon-healthcare-assessment',
-    goal: -1,
-    translation_key: 'targets.tb_test.title',
-    subtitle_translation_key: 'targets.this_month.subtitle',
-
-    appliesTo: 'reports',
-    date: 'reported'
-  },
-{
-    id: 'ag_test',
-    type: 'count',
-    icon: 'icon-healthcare-assessment',
-    goal: -1,
-    translation_key: 'targets.ag_test.title',
-    subtitle_translation_key: 'targets.this_month.subtitle',
-  
-    appliesTo: 'reports',
+    appliesTo: 'contacts',
+    appliesToType: ['person'],
+    appliesIf: covidPercTested,
     date: 'reported'
   },
 
