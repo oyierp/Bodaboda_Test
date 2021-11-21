fd3f7b4249a323c721f9c17383e7280b3b7ba4e3
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2021-11-22 01:05:25 +0300

    Add files via upload

--- config-boda-moh-ke-master/nools-extras.js
+++ config-boda-moh-ke-master/nools-extras.js
@@ -1,9 +1,11 @@
-function covidPercTested(contact) {
-  return contact.reports.some(report => report.form === 'screening' && report.fields.cov === 'true');
-}
+module.exports = {
+  function covidPercTested(contact) {
+    return contact.reports.some(report => report.form === 'screening' && report.fields.cov === 'true');
+  }
 
 module.exports = {
-covidPercTested
+  covidPercTested,
+  },
 };
 
 
