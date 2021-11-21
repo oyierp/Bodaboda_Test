30394f37caa66043aea9c5979454fa3dd2b9f23d
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2021-11-22 00:21:38 +0300

    Add files via upload

--- config-boda-moh-ke-master/targets.js
+++ config-boda-moh-ke-master/targets.js
@@ -2,24 +2,24 @@ const extras = require('./nools-extras');
 
 const {
   covidPercTested,
-  } = extras; 
+  }  = extras; 
 
 module.exports = [
-{
-  id: 'percentage-covid-test',
-  type: 'percent',
-  icon: 'icon-healthcare-assessment',
-  goal: -1,
-  translation_key: 'targets.covid.title',
-  subtitle_translation_key: 'targets.this_month.subtitle',
+  {
+    id: 'percentage-covid-test',
+    type: 'percent',
+    icon: 'icon-healthcare-assessment',
+    goal: -1,
+    translation_key: 'targets.covid.title',
+    subtitle_translation_key: 'targets.this_month.subtitle',
 
-  appliesTo: 'contacts',
-  appliesToType: ['person'],
+    appliesTo: 'contacts',
+    appliesToType: ['person'],
  
-  passesIf: covidPercTested,
+    passesIf: covidPercTested,
  
-  date:'reported',
-},
+    date:'reported',
+  },
 
 ];
 
