81f64da61d74da2810c7e517b7f621f91a6cd39a
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2021-11-22 15:04:40 +0300

    Add files via upload

--- config-boda-moh-ke-master/nools-extras.js
+++ config-boda-moh-ke-master/nools-extras.js
@@ -1,6 +1,14 @@
 module.exports = {
   covidPercTested(contact) {
     return contact.reports.some(report => report.form === 'screening' && report.fields.cov === 'true');
+  },
+
+  tbTested(contact) {
+    return contact.reports.some(report => report.form === 'screening' && report.fields.tb === 'true');
+  },
+
+pcrPercTested(contact) {
+    return contact.reports.some(report => report.form === 'screening' && report.fields.rtpcr === 'true');
   }
 
 };
--- config-boda-moh-ke-master/targets.js
+++ config-boda-moh-ke-master/targets.js
@@ -1,15 +1,16 @@
 const extras = require('./nools-extras');
 
-const 
-  {
+const {
     covidPercTested,
+    tbPercTested,
+    pcrPercTested,
   } = extras; 
 
 module.exports = [
   {
     id: 'percentage-covid-test',
     type: 'percent',
-    icon: 'icon-healthcare-assessment',
+    icon: 'covid-case',
     goal: -1,
     translation_key: 'targets.covid.title',
     subtitle_translation_key: 'targets.this_month.subtitle',
@@ -18,6 +19,38 @@ module.exports = [
     appliesToType: ['person'],
  
     passesIf: covidPercTested,
+ 
+    date:'reported',
+  },
+
+{
+    id: 'percentage-tb-test',
+    type: 'percent',
+    icon: 'icon-people-person-general@2x',
+    goal: -1,
+    translation_key: 'targets.tb.title',
+    subtitle_translation_key: 'targets.this_month.subtitle',
+
+    appliesTo: 'contacts',
+    appliesToType: ['person'],
+ 
+    passesIf: tbPercTested,
+ 
+    date:'reported',
+  },
+
+{
+    id: 'percentage-pcr-test',
+    type: 'percent',
+    icon: 'icon-people-man',
+    goal: -1,
+    translation_key: 'targets.covid.title',
+    subtitle_translation_key: 'targets.this_month.subtitle',
+
+    appliesTo: 'contacts',
+    appliesToType: ['person'],
+ 
+    passesIf: pcrPercTested,
  
     date:'reported',
   },
