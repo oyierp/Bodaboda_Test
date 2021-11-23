6801189c8983ff2cfba5b492b8b74aade4e25024
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2021-11-23 13:10:13 +0300

    Add files via upload

--- config-boda-moh-ke-master/targets.js
+++ config-boda-moh-ke-master/targets.js
@@ -10,7 +10,7 @@ module.exports = [
     id: 'percentage-positive-covid-test',
     type: 'percent',
     icon: 'medic-covid-case',
-    goal: -1,
+    goal: 10,
     translation_key: 'targets.posCovid.title',
     subtitle_translation_key: 'targets.this_month.subtitle',
 
@@ -26,7 +26,7 @@ module.exports = [
     id: 'percentage-negative-covid-test',
     type: 'percent',
     icon: 'medic-covid-case',
-    goal: -1,
+    goal: 10,
     translation_key: 'targets.negCovid.title',
     subtitle_translation_key: 'targets.this_month.subtitle',
 
