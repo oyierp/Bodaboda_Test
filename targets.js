2f7bc66f62331ff98487ea5d5e6a41c965c1d2d7
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2021-12-01 13:12:25 +0300

    Add files via upload

---
+++ targets.js
@@ -0,0 +1,38 @@
+const extras = require('./nools-extras');
+
+const {
+  posCovidPercTested,
+  negCovidPercTested,
+  skipChw,
+} = extras; 
+
+module.exports = [
+  {
+    id: 'percentage-positive-covid-test',
+    type: 'percent',
+    icon: 'medic-covid-case',
+    goal: -1,
+    translation_key: 'targets.posCovid.title',
+    subtitle_translation_key: 'targets.this_month.subtitle',
+    appliesTo: 'contacts',
+    appliesToType: ['person'],
+    appliesIf: skipChw,
+    passesIf: posCovidPercTested,
+    date:'reported',
+  },
+  
+  {
+    id: 'percentage-negative-covid-test',
+    type: 'percent',
+    icon: 'medic-covid-case',
+    goal: -1,
+    translation_key: 'targets.negCovid.title',
+    subtitle_translation_key: 'targets.this_month.subtitle',
+    appliesTo: 'contacts',
+    appliesToType: ['person'],
+    appliesIf: skipChw,
+    passesIf: negCovidPercTested,
+    date:'reported',
+  },
+
+];
