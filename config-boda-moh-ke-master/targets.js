68e907321a5b04282293e5fc861a454a3ac1ad7d
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2021-12-06 17:18:00 +0300

    Add files via upload

--- config-boda-moh-ke-master/targets.js
+++ config-boda-moh-ke-master/targets.js
@@ -3,31 +3,47 @@ const extras = require('./nools-extras');
 const {
   posCovidPercTested,
   negCovidPercTested,
+  dateVisited,
 } = extras; 
 
 module.exports = [  
   {
-    id: 'percentage-positive-covid-test',
-    type: 'count',
+    id: 'posCovid-test-all-time',
+    type: 'percent',
+    icon: 'medic-covid-case',
+    goal: -1,
+    translation_key: 'targets.posCovid.title',
+    subtitle_translation_key: 'targets.all_time.subtitle',
+    appliesTo: 'contacts',
+    appliesToType: ['person'],
+    idType: tentVisitDates,
+    appliesIf: posCovidPercTested,
+    date:'now',
+  },
+
+  {
+    id: 'posCovid-test-this-month',
+    type: 'percent',
     icon: 'medic-covid-case',
     goal: -1,
     translation_key: 'targets.posCovid.title',
     subtitle_translation_key: 'targets.this_month.subtitle',
     appliesTo: 'contacts',
     appliesToType: ['person'],
-    appliesIf: posCovidPercTested,
-    date:'now',
-  },
+    idType: tentVisitDates,
+    date: 'reported'
+  }
 
   {
-    id: 'percentage-negative-covid-test',
-    type: 'count',
+    id: 'negCovid-test-all-time',
+    type: 'percent',
     icon: 'medic-covid-case',
     goal: -1,
     translation_key: 'targets.negCovid.title',
     subtitle_translation_key: 'targets.this_month.subtitle',
     appliesTo: 'contacts',
     appliesToType: ['person'],
+    idType: tentVisitDates,
     appliesIf: negCovidPercTested,
     date:'now',
   },
