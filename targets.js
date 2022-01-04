73d668625b8d30b8649452aa7a7174e6829a4d25
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2022-01-04 18:07:27 +0300

    Add files via upload

---
+++ targets.js
@@ -0,0 +1,65 @@
+const extras = require('./nools-extras');
+
+const {
+  posCovidTested,
+  negCovidTested,
+  pendCovidTested,
+  incCovidTested,
+  emptCovidTested,
+  pendIncCovidTested,
+} = extras; 
+
+module.exports = [  
+  {
+    id: 'covid-test-all-time',
+    type: 'count',
+    icon: 'medic-covid-case',
+    goal: -1,
+    translation_key: 'targets.all_timeCovid.title',
+    subtitle_translation_key: 'targets.all_time.subtitle',
+    appliesTo: 'contacts',
+    appliesToType:['suspected_case'],
+    date:'now',
+    idType: 'contact'
+  },
+
+  {
+    id: 'posCovid-this-month',
+    type: 'count',
+    icon: 'medic-covid-case',
+    goal: -1,
+    translation_key: 'targets.posCovid.title',
+    subtitle_translation_key: 'targets.all_time.subtitle',
+    appliesTo: 'contacts',
+    appliesToType:['suspected_case'],
+    appliesIf:posCovidTested, 
+    date: 'now',
+  },
+
+  {
+    id: 'negCovid-this-month',
+    type: 'count',
+    icon: 'medic-covid-case',
+    goal: -1,
+    translation_key: 'targets.negCovid.title',
+    subtitle_translation_key: 'targets.all_time.subtitle',
+    appliesTo: 'contacts',
+    appliesToType:['suspected_case'],
+    appliesIf: negCovidTested,
+    date:'now',
+  },
+  
+  {
+    id: 'pendIcompCovid-test-all-time',
+    type: 'count',
+    icon: 'medic-covid-case',
+    goal: -1,
+    translation_key: 'targets.pendIcompCovid.title',
+    subtitle_translation_key: 'targets.all_time.subtitle',
+    appliesTo: 'contacts',
+    appliesToType:['suspected_case'],
+    appliesIf: pendIncCovidTested,
+    date:'now',
+  },
+    
+];
