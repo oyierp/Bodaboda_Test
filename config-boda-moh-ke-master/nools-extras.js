1081686f1c2af2cd05ddc7cc8ce82515f69d902e
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2022-09-02 14:20:25 +0300

    Add files via upload

--- config-boda-moh-ke-master/nools-extras.js
+++ config-boda-moh-ke-master/nools-extras.js
@@ -23,42 +23,42 @@ const emptCovidTested = (contact) => {
 
 const mksCovidTested = (contact) => {
   return contact.parent && 
-    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === 'be17a990-9b88-47b0-9b86-d99fa9674768') && report.fields.testing && report.fields.testing.cov_test);
+    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === 'a2df09b4-6942-48a4-bda4-e730bd3e9e6e') && report.fields.testing && report.fields.testing.cov_test);
 };
 
 const kjdCovidTested = (contact) => {
   return contact.parent && 
-    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === '290cddae-3b9a-435b-82f0-40bb4614403d') && report.fields.testing && report.fields.testing.cov_test);
+    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === '792ab916-1422-499c-82df-abf19afd33e5') && report.fields.testing && report.fields.testing.cov_test);
 };
 
 const nrbCovidTested = (contact) => {
   return contact.parent && 
-    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === 'ed8fa317-2a47-478c-953d-894bfe0ab011') && report.fields.testing && report.fields.testing.cov_test);
+    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === '8d4c38d1-d946-4652-ad19-c59639512958') && report.fields.testing && report.fields.testing.cov_test);
 };
 
-const kbuCovidTested = (contact) => {
+const msaCovidTested = (contact) => {
   return contact.parent && 
-    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === 'f3f2cd81-e0d7-452d-b4fa-e23cfa0820a3') && report.fields.testing && report.fields.testing.cov_test);
+    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === 'f32fcac5-9395-4e64-8da9-2b86d4474e0f') && report.fields.testing && report.fields.testing.cov_test);
 };
 
 const posCovidTestedMks = (contact) => {
   return contact.parent && 
-    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === 'be17a990-9b88-47b0-9b86-d99fa9674768') && report.fields.testing && report.fields.testing.cov_test ==='Positive');
+    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === 'a2df09b4-6942-48a4-bda4-e730bd3e9e6e') && report.fields.testing && report.fields.testing.cov_test ==='Positive');
 };
 
 const posCovidTestedKjd = (contact) => {
   return contact.parent && 
-    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === '290cddae-3b9a-435b-82f0-40bb4614403d') && report.fields.testing && report.fields.testing.cov_test ==='Positive');
+    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === '792ab916-1422-499c-82df-abf19afd33e5') && report.fields.testing && report.fields.testing.cov_test ==='Positive');
 };
 
 const posCovidTestedNrb = (contact) => {
   return contact.parent && 
-    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === 'ed8fa317-2a47-478c-953d-894bfe0ab011') && report.fields.testing && report.fields.testing.cov_test ==='Positive');
+    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === '8d4c38d1-d946-4652-ad19-c59639512958') && report.fields.testing && report.fields.testing.cov_test ==='Positive');
 };
 
-const posCovidTestedKbu = (contact) => {
+const posCovidTestedMsa = (contact) => {
   return contact.parent && 
-    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === 'f3f2cd81-e0d7-452d-b4fa-e23cfa0820a3') && report.fields.testing && report.fields.testing.cov_test ==='Positive');
+    contact.reports.some(report => (report.form === 'case_investigation' && report.contact.parent && report.contact.parent._id === 'f32fcac5-9395-4e64-8da9-2b86d4474e0f') && report.fields.testing && report.fields.testing.cov_test ==='Positive');
 };
 
 module.exports = {
@@ -71,11 +71,11 @@ module.exports = {
   mksCovidTested,
   kjdCovidTested,
   nrbCovidTested,
-  kbuCovidTested,
+  msaCovidTested,
   posCovidTestedMks,
   posCovidTestedKjd,
   posCovidTestedNrb,
-  posCovidTestedKbu
+  posCovidTestedMsa
 
 }; 
 
